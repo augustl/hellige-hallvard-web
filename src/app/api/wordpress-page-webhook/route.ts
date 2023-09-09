2e2797f7289e9c6e80e55cb8acdbdcb0c1a8b1c9
August Lilleaas <august@augustl.com>
2023-09-09 22:58:51 +0200

    Debug WP form data webhook output

--- src/app/api/wordpress-page-webhook/route.ts
+++ src/app/api/wordpress-page-webhook/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
+    const wpData = await request.formData()
     console.log("---- got wp page")
-    console.log(await request.text())
+    console.log(wpData.get("ID"))
+    console.log(wpData.get("guid"))
+    console.log(wpData.get("post_status"))
+    console.log(wpData.get("post_type"))
+    console.log(wpData.get("post_url"))
+    console.log(wpData.get("reader_url"))
     return new NextResponse() 
 }
