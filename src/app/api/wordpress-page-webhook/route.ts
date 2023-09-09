d86d3301526471cc88c3fa6dcc97b09fb87f8323
August Lilleaas <august@augustl.com>
2023-09-09 22:45:41 +0200

    Add webhook test for wordpress

---
+++ src/app/api/wordpress-page-webhook/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+    console.log("---- got wp page")
+    console.log(await request.json())
+    return new NextResponse() 
+}
---
+++ src/app/api/wordpress-post-webhook/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+    console.log("---- got wp post")
+    console.log(await request.json())
+    return new NextResponse() 
+}
