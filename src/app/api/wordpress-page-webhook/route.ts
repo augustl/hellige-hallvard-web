66ffd66148e03ee423b8069f3265677787dd8149
August Lilleaas <august@augustl.com>
2023-09-09 22:55:55 +0200

    Debug wordpress webhook - apparently we don't get JSON

--- src/app/api/wordpress-page-webhook/route.ts
+++ src/app/api/wordpress-page-webhook/route.ts
@@ -2,6 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
     console.log("---- got wp page")
-    console.log(await request.json())
+    console.log(await request.text())
     return new NextResponse() 
 }
