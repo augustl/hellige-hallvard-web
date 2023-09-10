6bc1ac15efaa8ae7fda6a98404e90fe5385a8469
August Lilleaas <august@augustl.com>
2023-09-10 17:56:42 +0200

    Revalidate home page cache in webhook

--- src/app/api/wordpress-page-webhook/route.ts
+++ src/app/api/wordpress-page-webhook/route.ts
@@ -3,15 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
     const wpData = await request.formData()
-    console.log("-----")
-    console.log(wpData)
-    console.log(process.env.NEXT_PUBLIC_HOME_PAGE_ID)
-    console.log(process.env.NEXT_PUBLIC_HOME_PAGE_ID === wpData.get("ID"))
     const postUrl = wpData.get("post_url") as string
     const pageSlug = postUrl.replace(`https://${process.env.NEXT_PUBLIC_WORDPRESS_URL}`, "").replace(/\/$/, "")
     console.log(`Invalidating cache for path ${pageSlug}`)
-    // TODO: Revalidate home page when page id is process.env.NEXT_PUBLIC_HOME_PAGE_ID
     revalidatePath(pageSlug)
+
+    if (process.env.NEXT_PUBLIC_HOME_PAGE_ID === wpData.get("ID")) {
+        console.log("Invalidating cache for path /")
+        revalidatePath("/")
+    }
 
     return new NextResponse() 
 }
