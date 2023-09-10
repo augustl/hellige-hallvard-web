c881a83dee6b32397c67d40254bba553aa0f9a37
August Lilleaas <august@augustl.com>
2023-09-10 17:51:23 +0200

    Fix trailing slash in page webhook cache invalidation
    Also log to check for TODO for re-validating home page

--- src/app/api/wordpress-page-webhook/route.ts
+++ src/app/api/wordpress-page-webhook/route.ts
@@ -3,8 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
     const wpData = await request.formData()
+    console.log("-----")
+    console.log(wpData)
+    console.log(wpData.get("ID"))
+    console.log(process.env.NEXT_PUBLIC_HOME_PAGE_ID)
     const postUrl = wpData.get("post_url") as string
-    const pageSlug = postUrl.replace(`https://${process.env.NEXT_PUBLIC_WORDPRESS_URL}`, "")
+    const pageSlug = postUrl.replace(`https://${process.env.NEXT_PUBLIC_WORDPRESS_URL}`, "").replace(/\/$/, "")
     console.log(`Invalidating cache for path ${pageSlug}`)
     // TODO: Revalidate home page when page id is process.env.NEXT_PUBLIC_HOME_PAGE_ID
     revalidatePath(pageSlug)
