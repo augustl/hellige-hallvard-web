0730da3c9b2933c79ab0a63c63f7e396b92e4537
August Lilleaas <august@augustl.com>
2023-09-10 17:54:39 +0200

    Add logging of equality to check for str/number etc

--- src/app/api/wordpress-page-webhook/route.ts
+++ src/app/api/wordpress-page-webhook/route.ts
@@ -5,8 +5,8 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     const wpData = await request.formData()
     console.log("-----")
     console.log(wpData)
-    console.log(wpData.get("ID"))
     console.log(process.env.NEXT_PUBLIC_HOME_PAGE_ID)
+    console.log(process.env.NEXT_PUBLIC_HOME_PAGE_ID === wpData.get("ID"))
     const postUrl = wpData.get("post_url") as string
     const pageSlug = postUrl.replace(`https://${process.env.NEXT_PUBLIC_WORDPRESS_URL}`, "").replace(/\/$/, "")
     console.log(`Invalidating cache for path ${pageSlug}`)
