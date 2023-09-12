f9ed76dcb254caedfdeed5121dda187092c1e6bf
August Lilleaas <august@augustl.com>
2023-09-12 13:41:03 +0200

    Ensure layout page lists cache is invalidated on webhook

--- src/app/api/wordpress-page-webhook/route.ts
+++ src/app/api/wordpress-page-webhook/route.ts
@@ -7,6 +7,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     const pageKey = postUrl.replace(`https://${process.env.NEXT_PUBLIC_WORDPRESS_URL}/`, "").replace(/\/$/, "")
     console.log(`Invalidating cache for path ${pageKey}`)
     revalidateTag(`wp-page-${pageKey}`)
+    revalidateTag(`wp-pages`)
 
     if (process.env.NEXT_PUBLIC_HOME_PAGE_ID === wpData.get("ID")) {
         console.log("Invalidating cache for root page")
