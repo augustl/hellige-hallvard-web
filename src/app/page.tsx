0bd9ad70c10ccdaa76ceccd181c8d19ba271c96f
August Lilleaas <august@augustl.com>
2023-09-12 12:38:02 +0200

    Make home page news summary more prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+const vowels = new Set(["a", "e", "i", "o", "u", "æ", "ø", "å", "A", "E", "I", "O", "U", "Æ", "Ø", "Å"])
+
+// Really super long words won't always break automatically in Safari, so we tell it to break before any vocal
+// which seems to work reasonably well.
+const wordbreakify = (text: string) => {
+    const result: string[] = []
+    let curr: string[] = []
+    for (const char of text) {
+        if (vowels.has(char)) {
+            curr.length > 0 && result.push(curr.join(""))
+            curr = [char]
+        } else {
+            curr.push(char)
+        }
+    }
+
+    curr.length > 0 && result.push(curr.join(""))
+
+
+    return result.map(it => `${it}${String.fromCharCode(173)}`).join("")
+}
+
 export default async function HomePage() {
     const wpPagesDataReq = fetch(`https://public-api.wordpress.com/wp/v2/sites/${process.env.NEXT_PUBLIC_WORDPRESS_URL}/pages?context=view&include=${process.env.NEXT_PUBLIC_HOME_PAGE_ID}`, {next: {tags: ["wp-home-page"]}})
     const wpPostsDataReq = fetch(`https://public-api.wordpress.com/wp/v2/sites/${process.env.NEXT_PUBLIC_WORDPRESS_URL}/posts?context=embed&per_page=3`, {next: {tags: ["wp-home-page", "wp-posts"]}})
@@ -18,7 +40,7 @@ export default async function HomePage() {
 
                     return <li key={it.id} className="flex flex-row gap-4 mb-4 items-center">
                         <span>{d}.{m}.{y}</span>
-                        <a href={`/nyheter/${y}/${m}/${it.slug}`} className="uppercase font-bold text-2xl font-serif hyphens-auto">{it.title.rendered}</a>
+                        <a href={`/nyheter/${y}/${m}/${it.slug}`} className="uppercase font-bold text-2xl font-serif hyphens-auto">{wordbreakify(it.title.rendered)}</a>
                     </li>
                 })}
             </ul>
