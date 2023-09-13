392c344e19015e367d9cd57b6574ed798a3e5083
August Lilleaas <august@augustl.com>
2023-09-13 12:05:03 +0200

    Improve news pagination on mobile

--- src/app/nyheter/side/[pageNum]/page.tsx
+++ src/app/nyheter/side/[pageNum]/page.tsx
@@ -2,10 +2,10 @@ import { notFound } from "next/navigation"
 
 const NewsPagination: React.FC<{currentPage: number, totalNumPages: number}> = ({currentPage, totalNumPages}) => {
     return <div className="hh-content-blocks hh-body-typography mt-10">
-        <div className="grid" style={{gridTemplateColumns: "1fr auto 1fr"}}>
+        <div className="grid gap-4" style={{gridTemplateColumns: "1fr auto 1fr"}}>
             <div className="flex flex-row gap-4">
                 {currentPage !== 1 && <>
-                    <a href={`/nyheter/side/1`}>Første side</a>
+                    <a href={`/nyheter/side/1`} className="hidden md:block">Første side</a>
                     <a href={`/nyheter/side/${currentPage - 1}`}>Forrige side</a>
                 </>}
             </div>
@@ -16,7 +16,7 @@ const NewsPagination: React.FC<{currentPage: number, totalNumPages: number}> = (
             <div className="flex flex-row gap-4 ml-auto">
                 {currentPage !== totalNumPages && <>
                     <a href={`/nyheter/side/${currentPage + 1}`}>Neste side</a>
-                    <a href={`/nyheter/side/${totalNumPages}`}>Siste side</a>
+                    <a href={`/nyheter/side/${totalNumPages}`} className="hidden md:block">Siste side</a>
                 </>}
             </div>
         </div>
