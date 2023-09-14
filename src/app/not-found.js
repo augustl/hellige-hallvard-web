7473adbfd5af54960da4c7f5d9f3f9c21de3029e
August Lilleaas <august@augustl.com>
2023-09-14 13:15:43 +0200

    Improve text on 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,3 +1,7 @@
 export default function NotFound() {
-    return <div>Not found page is here</div>
+    return <div className="hh-typography hh-body-typography hh-content-blocks text-center">
+        <h1>Siden finnes ikke</h1>
+        <p>Desverre er det ikke noe å vise på denne siden.</p>
+        <p>(404)</p>
+    </div>
 }
