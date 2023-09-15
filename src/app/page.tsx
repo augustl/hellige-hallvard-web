8ac335f483db6ddb0e958c5b6fef9806380fff32
August Lilleaas <august@augustl.com>
2023-09-15 09:22:37 +0200

    No need to wordbreakify with new layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,5 @@
 import UpcomingEventsListSSR from "@/components/UpcomingEventsListSSR"
 
-const vowels = new Set(["a", "e", "i", "o", "u", "æ", "ø", "å", "A", "E", "I", "O", "U", "Æ", "Ø", "Å"])
-
-// Really super long words won't always break automatically in Safari, so we tell it to break before any vocal
-// which seems to work reasonably well.
-const wordbreakify = (text: string) => {
-    const result: string[] = []
-    let curr: string[] = []
-    for (const char of text) {
-        if (vowels.has(char)) {
-            curr.length > 0 && result.push(curr.join(""))
-            curr = [char]
-        } else {
-            curr.push(char)
-        }
-    }
-
-    curr.length > 0 && result.push(curr.join(""))
-
-
-    return result.map(it => `${it}${String.fromCharCode(173)}`).join("")
-}
-
 export default async function HomePage() {
     const [wpPagesDataRes, wpPostsDataRes] = await Promise.all([
         fetch(`https://public-api.wordpress.com/wp/v2/sites/${process.env.NEXT_PUBLIC_WORDPRESS_URL}/pages?context=view&include=${process.env.NEXT_PUBLIC_HOME_PAGE_ID}`, {next: {tags: ["wp-home-page"]}}),
@@ -51,7 +29,7 @@ export default async function HomePage() {
 
                                 return <li key={it.id} className="flex flex-row gap-4 mb-4 items-center">
                                     <span>{d}.{m}.{y}</span>
-                                    <a href={`/nyheter/${y}/${m}/${it.slug}`} className="font-bold hyphens-auto w-full" dangerouslySetInnerHTML={{__html: wordbreakify(it.title.rendered)}}></a>
+                                    <a href={`/nyheter/${y}/${m}/${it.slug}`} className="font-bold hyphens-auto w-full" dangerouslySetInnerHTML={{__html: it.title.rendered}}></a>
                                 </li>
                             })}
                         </ul>
