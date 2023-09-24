e524c9081da83dc3734512c80a9cf3347f9005fe
August Lilleaas <august@augustl.com>
2023-09-24 19:33:43 +0200

    Fix menu in dark mode

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,8 +11,8 @@ const Menu: React.FC<{wpPagesData: any[]}> = ({wpPagesData}) => {
 
 
     return <div className="w-full flex flex-col items-center">
-        <button className="visible md:hidden font-bold uppercase bg-gray-200 px-6 py-4" onClick={e => setMenuVisible(!menuVisible)}>Meny</button>
-        <ul className={`${menuVisible ? 'visible' : 'hidden'} md:flex flex-col md:flex-row md:gap-5 bg-gray-200 md:bg-transparent w-full md:w-auto`}>
+        <button className="visible md:hidden font-bold uppercase bg-gray-200 text-black px-6 py-4" onClick={e => setMenuVisible(!menuVisible)}>Meny</button>
+        <ul className={`${menuVisible ? 'visible' : 'hidden'} md:flex flex-col md:flex-row md:gap-5 bg-gray-200 text-black md:bg-transparent w-full md:w-auto`}>
             <li><NavLink href="/">Velkommen</NavLink></li>
             <li><NavLink href="/nyheter/side/1">Nyheter</NavLink></li>
             {wpPagesData.map(it => {
