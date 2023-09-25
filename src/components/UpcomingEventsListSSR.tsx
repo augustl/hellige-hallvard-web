1fae44941d66d956a8c311f8154009bd4c16af8b
August Lilleaas <august@augustl.com>
2023-09-25 20:26:14 +0200

    Group upcoming events by date for space and readability

--- src/components/UpcomingEventsListSSR.tsx
+++ src/components/UpcomingEventsListSSR.tsx
@@ -1,3 +1,5 @@
+import { partitionBy } from "@/utils"
+
 const calendarFormatter = new Intl.DateTimeFormat("nb-NO", {
     weekday: 'long',
     day: 'numeric',
@@ -7,18 +9,42 @@ const calendarFormatter = new Intl.DateTimeFormat("nb-NO", {
     timeZone: "Europe/Oslo"
 })
 
+const NUM_DATES = 3
+
+const DateHeadline: React.FC<{date: Date}> = ({date}) => {
+    const dateParts = calendarFormatter.formatToParts(date)
+    const weekday = dateParts.filter(it => it.type === "weekday")[0].value
+    const month = dateParts.filter(it => it.type === "month")[0].value
+    const day = dateParts.filter(it => it.type === "day")[0].value
+
+    return <><span className="capitalize">{weekday}</span> {day}. {month}</>
+}
+
 export default async function UpcomingEventsListSSR() {
     const calendarCutoffTime = new Date()
     calendarCutoffTime.setHours(0, 0, 0, 0)
-    const res = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(process.env.NEXT_PUBLIC_GCAL_ID!)}/events?key=${process.env.NEXT_PUBLIC_GCAL_BACKEND_API_KEY!}&maxResults=4&timeMin=${calendarCutoffTime.toISOString()}&timeZone=Europe/Oslo&orderBy=startTime&singleEvents=true`, {next: {revalidate: 60}})
+    const res = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(process.env.NEXT_PUBLIC_GCAL_ID!)}/events?key=${process.env.NEXT_PUBLIC_GCAL_BACKEND_API_KEY!}&maxResults=8&timeMin=${calendarCutoffTime.toISOString()}&timeZone=Europe/Oslo&orderBy=startTime&singleEvents=true`, {next: {revalidate: 60}})
     const upcomingEventsData: {id: number, start: {dateTime: string}, summary: string}[] = (await res.json()).items
+    const upcomingEvents = upcomingEventsData.map(it => {
+        const start = new Date(it.start.dateTime)
+        return {...it, date: start, dateKey: `${start.getFullYear()}-${start.getMonth()}-${start.getDate()}`}
+    })
 
-    return <ul>
-        {upcomingEventsData.map((event, idx) => {
-            return <li key={event ? event.id : `fake-${idx}`} className="mb-4">
-                <div className="font-bold">{event.summary.replace(/\.[\s+]?$/, "")}</div>
-                <div className="whitespace-nowrap">{calendarFormatter.format(new Date(event.start.dateTime))}</div>
-            </li>
+    const upcomingEventsByDay = partitionBy(upcomingEvents, it => it.dateKey).slice(0, NUM_DATES)
+
+    return <div className="flex flex-col gap-4 mb-4">
+        {upcomingEventsByDay.map((events, idx) => {
+            return <div key={events[0].dateKey} className={(idx === (NUM_DATES - 1)) ? "hidden md:block" : ""}>
+                <h3 className="font-bold"><DateHeadline date={events[0].date} /></h3>
+                {events.map(event => {
+                    const dateParts = calendarFormatter.formatToParts(event.date)
+                    const hour = dateParts.filter(it => it.type === "hour")[0].value
+                    const minute = dateParts.filter(it => it.type === "minute")[0].value
+                    return <div key={event.id}>
+                        {hour}:{minute} {event.summary}
+                    </div>
+                })}
+            </div>
         })}
-    </ul>
+    </div>
 }
--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,33 @@ export const groupBy = <T>(xs: T[], f: (it: T) => string): {[key: string]: T[]} 
     }
     return res
 }
+
+export const partitionBy = <T>(xs: T[], f: (it: T) => unknown): T[][] => {
+    if (xs.length === 0) {
+        return []
+    }
+
+    if (xs.length === 1) {
+        return [xs]
+    }
+
+    const res: T[][] = []
+    let prevF = f(xs[0])
+    let curr: T[] = [xs[0]]
+    for (let i = 1; i < xs.length; i++) {
+        const x = xs[i]
+        const nextF = f(x)
+        if (prevF === nextF) {
+            curr.push(x)
+        } else {
+            res.push(curr)
+            curr = [x]
+        }
+
+        prevF = nextF
+    }
+
+    res.push(curr)
+
+    return res
+}
