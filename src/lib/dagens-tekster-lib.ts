01c1f0d007f28b1c534c1c91fdd3ec562bcb8edc
August Lilleaas <august@augustl.com>
2024-01-02 15:13:46 +0100

    Cache call to getDagensTekster

--- src/lib/dagens-tekster-lib.ts
+++ src/lib/dagens-tekster-lib.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { cache } from 'react'
 import { DagensTekstItems } from "@/types/dynamodb"
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
 import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb"
@@ -15,7 +16,7 @@ type DagensTekstDynamoItems = {
 }[]
 
 
-export const getDagensTekster = async (y: string, m: string, d: string): Promise<DagensTekstItems | null> => {
+export const getDagensTekster = cache(async (y: string, m: string, d: string): Promise<DagensTekstItems | null> => {
     const sk = `${y}-${m.padStart(2, '0')}-${d.padStart(2, '0')}`
 
     const {Item: item} = await docClient.send(new GetCommand({
@@ -31,7 +32,7 @@ export const getDagensTekster = async (y: string, m: string, d: string): Promise
     }
 
     return processDagensTekster((item as any).items as DagensTekstDynamoItems)
-}
+})
 
 export const processDagensTekster = (items: DagensTekstDynamoItems): DagensTekstItems  => {
     return items.map(dagensTekstItem => {
