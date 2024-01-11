824d458ea5c94ca0793bb4a826094aa26a17e21f
August Lilleaas <august@augustl.com>
2024-01-11 22:32:54 +0100

    Add some logging to see if this code is invoked always

--- src/lib/nb88-fetch-lib.ts
+++ src/lib/nb88-fetch-lib.ts
@@ -4,10 +4,13 @@ import { cache } from "react"
 
 export const fetchChapterFromNB88 = cache(async (book: unknown, chapter: unknown): Promise<string> => {
     const url = `http://les.norsk-bibel.no/index_reader.php?res=${book}:${chapter}`
+    console.log("Fetching from", url)
 
     const res = await fetch(
         url,
         {headers: {"X-Hei-Fra-Utvikler": "Hentet for helligehallvard.no. Vi betaler gjerne for et API :) Kontakt august@augustl.com"}}
     )
+
+    console.log("Performed fetch")
     return await res.text()
 })
