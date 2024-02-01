5363e3ddb5ce3540af32ab110c49b8a8b9fc29a1
August Lilleaas <august@augustl.com>
2024-02-01 15:10:07 +0100

    Maybe we get POST?

--- src/app/api/websub/[[...websub]]/route.ts
+++ src/app/api/websub/[[...websub]]/route.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
-    console.log("-------")
-    console.log(request.nextUrl.pathname)
+    console.log(`websub: GET ${request.nextUrl.pathname}`)
     console.log(request.nextUrl.searchParams)
 
     return new NextResponse() 
 }
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
+    console.log(`websub: POST ${request.nextUrl.pathname}`)
+    console.log(await request.text())
 
     return new NextResponse() 
 }
