97cffe13bb9f78cab4f030ab694726d8231efcc1
August Lilleaas <august@augustl.com>
2024-02-01 15:05:06 +0100

    Create catch all route for websub to see what's going on

--- src/app/api/websub/[[...websub]]/route.ts
+++ src/app/api/websub/[[...websub]]/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
     console.log("-------")
+    console.log(request.nextUrl.pathname)
     console.log(request.nextUrl.searchParams)
 
     return new NextResponse() 
