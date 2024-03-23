05b0a5d9c03fb1c9180f259f91b9bdc7c26c894d
August Lilleaas <august@augustl.com>
2024-03-23 12:22:04 +0100

    Clever nowrap setup

--- src/components/HomePageForDate.tsx
+++ src/components/HomePageForDate.tsx
@@ -36,8 +36,8 @@ export default async function HomePageForDate({date}: {date: moment.Moment}) {
             <div className="alignwide">
                 <div className="mb-8 gap-8 flex flex-col md:flex-row-reverse md:justify-end">
                     <div className="border border-yellow-600 border-double border-4 inline-block relative">
-                        <h2 className="px-4 py-2 bg-yellow-100 dark:bg-yellow-900 bg-opacity-40 text-xl whitespace-nowrap font-bold font-serif">
-                        𐡷 Hl. Efraim Syrerens bønn 𐡸
+                        <h2 className="px-4 py-2 bg-yellow-100 dark:bg-yellow-900 bg-opacity-40 text-xl font-bold font-serif">
+                        <span className="whitespace-nowrap">𐡷 Hl.</span> Efraim Syrerens <span className="whitespace-nowrap">bønn 𐡸</span>
                         </h2>
 
                         <div className="flex flex-col gap-4 p-4">
