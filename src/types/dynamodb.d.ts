101cb60e18a09c373eb9f80cc393659380ae0a57
August Lilleaas <august@augustl.com>
2024-06-30 20:35:40 +0200

    Whoops, add missing type update

--- src/types/dynamodb.d.ts
+++ src/types/dynamodb.d.ts
@@ -1,3 +1,3 @@
 export type DagensTekstItems = {book: string, items: DagensTekstItem[]}[]
-export type DagensTekstItem = {label: string, chapterChunks: DagensTekstItemChapterChunk[]}
+export type DagensTekstItem = {label: string, fullLabel: string, chapterChunks: DagensTekstItemChapterChunk[]}
 export type DagensTekstItemChapterChunk = {chapter: number, verseFrom: number, verseTo?: number}
