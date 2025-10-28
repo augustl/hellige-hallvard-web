40f75a7a2f66a26684b00c50914ed69b9cef658f
August Lilleaas <august@augustl.com>
2025-10-28 19:33:27 +0100

    Bump all deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,34 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    ...nextTs,
     {
         rules: {
             "@typescript-eslint/no-unused-vars": "off",
             "@typescript-eslint/no-explicit-any": "off",
             "@typescript-eslint/no-unused-expressions": "off"
-        },
+        }
     },
     {
         files: ["**/*.test.ts"],
         rules: {
             "prefer-const": "off"
-        },
-    }
-];
+        }
+    },
+    // Must be after other rules - automatically disables rules that conflicts with prettier
+    prettier,
+    globalIgnores([
+        'proto/**',
+        // Default ignores of eslint-config-next:
+        '.next/**',
+        'out/**',
+        'build/**',
+        'next-env.d.ts',
+    ])
+])
+
+export default eslintConfig
