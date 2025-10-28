679b93595c45e2c65e012e2fc59df52e02a9e775
August Lilleaas <august@augustl.com>
2025-10-28 19:50:21 +0100

    Update cache setup for next 16

--- src/app/api/wordpress-page-webhook/route.ts
+++ src/app/api/wordpress-page-webhook/route.ts
@@ -1,22 +1,25 @@
-import { revalidatePath, revalidateTag } from "next/cache";
-import { NextRequest, NextResponse } from "next/server";
+import {revalidatePath, revalidateTag} from "next/cache"
+import {NextRequest, NextResponse} from "next/server"
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
     const wpData = await request.formData()
     const postUrl = wpData.get("post_url") as string
     const postParent = wpData.get("post_parent") as string
-    const pathSegments = postUrl.replace(`https://${process.env.NEXT_PUBLIC_WORDPRESS_URL}/`, "").replace(/\/$/, "").split("/")
+    const pathSegments = postUrl
+        .replace(`https://${process.env.NEXT_PUBLIC_WORDPRESS_URL}/`, "")
+        .replace(/\/$/, "")
+        .split("/")
 
     for (const pageKey of pathSegments) {
         console.log(`Invalidating cache for path "${pageKey}"`)
-        revalidateTag(`wp-page-${pageKey}`)
+        revalidateTag(`wp-page-${pageKey}`, "max")
     }
-    revalidateTag(`wp-pages`)
+    revalidateTag(`wp-pages`, "max")
 
     if (process.env.NEXT_PUBLIC_HOME_PAGE_ID === wpData.get("ID")) {
         console.log("Invalidating cache for root page")
-        revalidateTag(`wp-home-page`)
+        revalidateTag(`wp-home-page`, "max")
     }
 
-    return new NextResponse() 
+    return new NextResponse()
 }
--- src/app/api/wordpress-webhook/[[...slug]]/route.ts
+++ src/app/api/wordpress-webhook/[[...slug]]/route.ts
@@ -1,16 +1,16 @@
-import { revalidateTag } from "next/cache";
-import { NextRequest, NextResponse } from "next/server";
+import {revalidateTag} from "next/cache"
+import {NextRequest, NextResponse} from "next/server"
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
     console.log("Invalidating all caches")
     const payload = await request.json()
 
-    revalidateTag("wp-posts")
-    revalidateTag(`wp-pages`)
-    revalidateTag(`wp-home-page`)
-    revalidateTag(`wp-all-pages-by-slug`)
+    revalidateTag("wp-posts", "max")
+    revalidateTag(`wp-pages`, "max")
+    revalidateTag(`wp-home-page`, "max")
+    revalidateTag(`wp-all-pages-by-slug`, "max")
 
     console.log("Done")
 
-    return new NextResponse() 
+    return new NextResponse()
 }
