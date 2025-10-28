831ba0d4298775db7e672c7c60f6e8fd980cf633
August Lilleaas <august@augustl.com>
2025-10-28 19:50:58 +0100

    More cache setup updates

--- src/app/api/reset-homepage-cache/route.ts
+++ src/app/api/reset-homepage-cache/route.ts
@@ -1,8 +1,8 @@
-import { revalidatePath, revalidateTag } from "next/cache";
-import { NextRequest, NextResponse } from "next/server";
+import {revalidatePath} from "next/cache"
+import {NextRequest, NextResponse} from "next/server"
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
     console.log(`Invalidating home page cache`)
     revalidatePath("/")
-    return new NextResponse() 
+    return new NextResponse()
 }
--- src/app/api/wordpress-post-webhook/route.ts
+++ src/app/api/wordpress-post-webhook/route.ts
@@ -1,9 +1,9 @@
-import { revalidatePath, revalidateTag } from "next/cache";
-import { NextRequest, NextResponse } from "next/server";
+import {revalidatePath, revalidateTag} from "next/cache"
+import {NextRequest, NextResponse} from "next/server"
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
     const wpData = await request.formData()
     console.log(`Invalidating posts cache`)
-    revalidateTag("wp-posts")
-    return new NextResponse() 
+    revalidateTag("wp-posts", "max")
+    return new NextResponse()
 }
